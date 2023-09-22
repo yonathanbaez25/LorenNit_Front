db80afe0d639cc01ae3c48bc86ca4163ed22ff9c
yonathanbaez25 <yacevedo170@gmail.com>
2023-09-22 08:37:44 -0400

    Longin and Register Page Completed

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,26 @@
+import "./Register.css";
+
+export default function Register() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">LorenNit Social App</h3>
+          <span className="loginDesc">
+            Connect with friends and the world around you on LorenNit
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Username" className="loginInput" />
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <input placeholder="Password Again" className="loginInput" />
+            <button className="loginButton">Sing Up</button>
+            <button className="loginRegisterButton">Log in Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
